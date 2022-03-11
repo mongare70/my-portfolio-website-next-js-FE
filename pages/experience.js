c6e41019e945c96310b6970ee5dc49b2194327d2
Hillary Mongare <hillarymongare70@gmail.com>
2022-03-11 11:20:07 +0300

    updated CV

--- pages/experience.js
+++ pages/experience.js
@@ -59,7 +59,7 @@ const Experience = () => {
           <br></br>
           <a
             className={classes.actions}
-            href="https://drive.google.com/file/d/1mc_GxgjYbdP2HQKuGIRjE5-WPPElwul3/view?usp=sharing"
+            href="https://drive.google.com/file/d/105g_5OQ9Twu6KMGB43_CDQ04KNAemv2U/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
