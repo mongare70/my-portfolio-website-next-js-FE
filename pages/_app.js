a302a2140513e4cd061f9a79aad04726a8e43edc
Hillary Mongare <hillarymongare70@gmail.com>
2022-05-08 14:10:02 +0300

    added more meta tags fir SEO

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,20 @@ function MyApp({ Component, pageProps }) {
     <Fragment>
       <Head>
         <title>Hillary Mongare Portfolio</title>
-        <link rel="icon" href="/hillary-oyaro-mongare-logo.jpg" />
+        <link rel="icon" href="%PUBLIC_URL%/hillary-oyaro-mongare-logo.jpg" />
         <meta
+          name="image"
+          property="og:image"
+          content="%PUBLIC_URL%/hillary-oyaro-mongare-logo.jpg"
+        />
+        <meta property="og:title" content="Hillary Oyaro Mongare Portfolio" />
+        <meta
+          property="og:description"
           name="description"
           content="Hi there, I'm Hillary Oyaro Mongare. Full-stack developer ready to turn your ideas into functional &amp; beautiful experiences"
         />
+        <meta name="author" content="Hillary Oyaro Mongare" />
+        <meta property="og:url" content="https://hillary-mongare.vercel.app/" />
       </Head>
       <Component {...pageProps} />
     </Fragment>
